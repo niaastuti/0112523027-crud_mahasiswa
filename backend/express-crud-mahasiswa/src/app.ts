2fd3d1fa583d953a62a8b78d016b0baf7114e60d
Nia Astuti <naynias@Nias-Macbook.local>
2026-06-22 11:54:18 +0700

    Initial progress backend CRUD mahasiswa

---
+++ backend/express-crud-mahasiswa/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mahasiswaRoutes from "./routes/mahasiswa.route";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API Express CRUD berjalan",
+  });
+});
+
+app.use("/api/mahasiswa", mahasiswaRoutes);
+
+export default app;
